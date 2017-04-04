9834c212839d3acc7bfb1a92d7904b335681cfaa
selvacodes <selvaganapathy@outlook.in>
2017-04-04 21:14:58 +0530

    still thunks

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,33 +1,40 @@
-function fakeAjax (url, cb) {
-  var fake_responses = {
-    'file1': 'The first text',
-    'file2': 'The middle text',
-    'file3': 'The last text'
-  }
-  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000
+function fakeAjax(url, cb) {
+	var fake_responses = {
+		'file1': 'The first text',
+		'file2': 'The middle text',
+		'file3': 'The last text'
+	}
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000
 
-  console.log('Requesting: ' + url)
+	console.log('Requesting: ' + url)
 
-  setTimeout(function () {
-    cb(fake_responses[url])
-  }, randomDelay)
+	setTimeout(function () {
+		cb(fake_responses[url])
+	}, randomDelay)
 }
 
-function output (text) {
-  console.log(text)
+function output(text) {
+	console.log(text)
 }
 
 // **************************************
 
-function getFile (file) {
-  return function (cb) {
-    return fakeAjax(file , console.log)
-  }
+function getFile(file) {
+	return function (cb) {
+		return fakeAjax(file, cb)
+	}
+}
+
+const txt = function (text) {
+	console.log("txt", text)
 }
 
 const file1 = getFile("file1")
 const file2 = getFile("file2")
 const file3 = getFile("file3")
 
-// request all files at once in "parallel"
+
+file1(txt)
+
+// request all files at once in "parallel
 // ???
