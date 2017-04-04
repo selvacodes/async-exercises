aae38aa0da209f441aee18903d6bbc20ee6f7018
selva <selvaganapathy@outlook.in>
2017-04-05 01:21:17 +0530

    thunks done

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -6,9 +6,10 @@ function fakeAjax(url, cb) {
 	}
 	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000
 
-	console.log('Requesting: ' + url)
+	console.log('Requesting: ' + url, randomDelay)
 
 	setTimeout(function () {
+		output("cb")
 		cb(fake_responses[url])
 	}, randomDelay)
 }
@@ -29,12 +30,57 @@ const txt = function (text) {
 	console.log("txt", text)
 }
 
-const file1 = getFile("file1")
-const file2 = getFile("file2")
-const file3 = getFile("file3")
+function getFileThunkfied(file) {
+
+	var state = {
+		value: null,
+		callb: null
+	}
+	var updateState = function (value) {
+		if (state.callb == null) {
+			state.value = value
+			return
+		}
+		state.callb(value)
+	}
 
 
-file1(txt)
+	var th = function (cb) {
+		if (state.value == null) {
+			state.callb = cb
+			return;
+		}
+		cb(state.value)
+	}
+
+	fakeAjax(file, updateState)
+
+	return function (cb) {
+		// console.log("in", cb)
+		th(cb)
+	}
+}
+
+
+var file1 = getFileThunkfied("file1")
+var file2 = getFileThunkfied("file2")
+var file3 = getFileThunkfied("file3")
+
+
+file1(function (text) {
+	output(text)
+	file2(function (text) {
+		output(text)
+		file3(function (text) {
+			output(text)
+			output("completed")
+		})
+
+	})
+
+})
+
+
 
 // request all files at once in "parallel
 // ???
